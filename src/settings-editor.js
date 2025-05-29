a5e985bab63b67f77ca38a923eb3cddc40d96400
BarryThePirate <barrythepirate@gmail.com>
2025-05-29 18:02:51 +0100

    updated settings logo

--- src/settings-editor.js
+++ src/settings-editor.js
@@ -1,32 +1,44 @@
 const iconSVG = `
 <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="black" width="24" height="24">
-  <!-- Skull and Crossbones Icon -->
-  <rect x="7" y="1" width="10" height="2"></rect>
-  <rect x="6" y="3" width="12" height="2"></rect>
-  <rect x="6" y="5" width="2" height="2"></rect>
-  <rect x="11" y="5" width="2" height="2"></rect>
-  <rect x="16" y="5" width="2" height="2"></rect>
-  <rect x="13" y="7" width="3" height="2"></rect>
-  <rect x="8" y="7" width="3" height="2"></rect>
-  <rect x="9" y="9" width="6" height="1"></rect>
-  <rect x="10" y="12" width="4" height="1"></rect>
-  <rect x="11" y="10" width="1" height="1"></rect>
-  <rect x="13" y="10" width="1" height="1"></rect>
-  <rect x="12" y="11" width="1" height="1"></rect>
-  <rect x="10" y="11" width="1" height="1"></rect>
-  <rect x="3" y="10" width="3" height="3"></rect>
-  <rect x="3" y="21" width="3" height="3"></rect>
-  <rect x="6" y="13" width="2" height="2"></rect>
-  <rect x="6" y="19" width="2" height="2"></rect>
-  <rect x="8" y="15" width="2" height="2"></rect>
-  <rect x="8" y="17" width="2" height="2"></rect>
-  <rect x="14" y="15" width="2" height="2"></rect>
-  <rect x="16" y="13" width="2" height="2"></rect>
-  <rect x="18" y="10" width="3" height="3"></rect>
-  <rect x="18" y="21" width="3" height="3"></rect>
-  <rect x="16" y="19" width="2" height="2"></rect>
-  <rect x="14" y="17" width="2" height="2"></rect>
-  <rect x="10" y="16" width="4" height="2"></rect>
+  <!-- Skull -->
+  <rect x="7" y="6" width="10" height="2"></rect>
+  <rect x="6" y="8" width="12" height="2"></rect>
+  <rect x="6" y="10" width="2" height="2"></rect>
+  <rect x="11" y="10" width="2" height="2"></rect>
+  <rect x="16" y="10" width="2" height="2"></rect>
+  <rect x="13" y="12" width="3" height="2"></rect>
+  <rect x="8" y="12" width="3" height="2"></rect>
+  <rect x="9" y="14" width="6" height="1"></rect>
+  <rect x="10" y="17" width="4" height="1"></rect>
+  <rect x="11" y="15" width="1" height="1"></rect>
+  <rect x="13" y="15" width="1" height="1"></rect>
+  <rect x="12" y="16" width="1" height="1"></rect>
+  <rect x="10" y="16" width="1" height="1"></rect>
+  
+  <!-- Crossbones -->
+  
+  <!-- Top Left -->
+  <rect x="2" y="0" width="2" height="2"></rect>
+  <rect x="0" y="2" width="4" height="2"></rect>
+  <rect x="4" y="4" width="2" height="2"></rect>
+  
+  <!-- Top Right -->
+  <rect x="20" y="0" width="2" height="2"></rect>
+  <rect x="20" y="2" width="4" height="2"></rect>
+  <rect x="18" y="4" width="2" height="2"></rect>
+  
+  <!-- Bottom Left -->
+  <rect x="0" y="20" width="4" height="2"></rect>
+  <rect x="2" y="22" width="2" height="2"></rect>
+  <rect x="4" y="18" width="2" height="2"></rect>
+  <rect x="6" y="16" width="2" height="2"></rect>
+  
+  <!-- Bottom Right -->
+  <rect x="20" y="20" width="4" height="2"></rect>
+  <rect x="20" y="22" width="2" height="2"></rect>
+  <rect x="18" y="18" width="2" height="2"></rect>
+  <rect x="16" y="16" width="2" height="2"></rect>
+  
 </svg>
 `;
 
